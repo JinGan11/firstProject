4ee32cb83ff32c618001e5ab9c123c5123ebb5b6
蓝庆裕 <qingyu.lan@ucarinc.com>
2019-08-07 17:29:01 +0800

    拦截器

---
+++ src/main/java/com/ucar/combination/config/WebMvcConfig.java
@@ -0,0 +1,27 @@
+package com.ucar.combination.config;
+
+import com.ucar.combination.config.interceptors.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+/**
+ * description:
+ * @author qingyu.lan@ucarinc.com
+ * @version 1.0
+ * @date: 2019-07-30 15:51:21
+ **/
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**").excludePathPatterns("/login/login.do_");
+    }
+}
---
+++ src/main/java/com/ucar/combination/config/interceptors/LoginInterceptor.java
@@ -0,0 +1,39 @@
+package com.ucar.combination.config.interceptors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * description:
+ * @author qingyu.lan@ucarinc.com
+ * @version 1.0
+ * @date: 2019-07-30 15:48:10
+ **/
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        HttpSession session = request.getSession();
+       if( session.getAttribute("accountId")==null){
+          // response.sendRedirect("/");
+           return false;
+       }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
