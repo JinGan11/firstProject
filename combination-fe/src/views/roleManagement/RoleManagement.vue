38e63742982212ae123d5252f063b7d69c215b32
jing.luo01@ucarinc.com <jing.luo01@ucarinc.com>
2019-08-08 16:52:11 +0800

    角色申请导出功能

--- combination-fe/src/views/roleManagement/RoleManagement.vue
+++ combination-fe/src/views/roleManagement/RoleManagement.vue
@@ -8,14 +8,14 @@
               <el-input style="width:200px;" v-model="form.name"></el-input>
             </el-form-item>
           </el-col>
-          <el-col :span ="3">
+          <el-col :span="3">
             <el-form-item>
               <el-button type="primary" @click="fetchData" style="width:100px">查询</el-button>
             </el-form-item>
           </el-col>
-          <el-col :span ="6">
+          <el-col :span="6">
             <el-form-item>
-              <el-button type="primary" style="width:100px" @click="">导出</el-button>
+              <el-button type="primary" style="width:100px" @click="exportRole">导出</el-button>
             </el-form-item>
           </el-col>
         </el-row>
@@ -29,23 +29,24 @@
       <el-button type="primary" @click="roleAssignPermission" style="width:100px">分配权限</el-button>
     </div>
 
-    <el-table ref="multipleTable" :data="tableData" border @selection-change="handleSelectionChange" >
+    <el-table ref="multipleTable" :data="tableData" border @selection-change="handleSelectionChange">
       <el-table-column label="选择" width="45">
         <template slot-scope="scope">
-          <el-radio v-model="selection" :label="scope.row.roleId" @change="selectionActive(scope.row)"><span width="0px;"></span></el-radio>
+          <el-radio v-model="selection" :label="scope.row.roleId" @change="selectionActive(scope.row)"><span
+            width="0px;"></span></el-radio>
         </template>
       </el-table-column>
       <el-table-column prop="id" v-if="false" label="隐藏id"></el-table-column>
       <el-table-column prop="roleId" label="角色ID" width="150"></el-table-column>
-      <el-table-column prop="roleName" label="角色名称"width="150"></el-table-column>
+      <el-table-column prop="roleName" label="角色名称" width="150"></el-table-column>
       <el-table-column prop="accountNum" label="审批人账号" width="120"></el-table-column>
-      <el-table-column prop="staffNum" label="审批人员工编号" ></el-table-column>
+      <el-table-column prop="staffNum" label="审批人员工编号"></el-table-column>
       <el-table-column prop="staffName" label="审批人姓名" width="200"></el-table-column>
       <el-table-column prop="departmentName" label="审批人所属部门" width="120"></el-table-column>
       <el-table-column prop="roleStatus" label="状态" width="100" style="text-align: center">
-         <template slot-scope="scope">
-           {{RoleStatusEnum[scope.row.roleStatus]}}
-          </template>
+        <template slot-scope="scope">
+          {{RoleStatusEnum[scope.row.roleStatus]}}
+        </template>
       </el-table-column>
       <el-table-column prop="description" label="描述" width="120"></el-table-column>
     </el-table>
@@ -59,12 +60,25 @@
                    layout="total, sizes, prev, pager, next, jumper"
                    :total="total">
     </el-pagination>
+    <el-dialog title='excelTitle' :visible.sync="exportDialogVisible" :close-on-click-modal="false" width="600px">
+      <template>
+        <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
+        <div style="margin: 15px 0;"></div>
+        <el-checkbox-group v-model="checkRoles" @change="handlecheckRolesChange">
+          <el-checkbox v-for="city in roles" :label="city" :key="city">{{city}}</el-checkbox>
+        </el-checkbox-group>
+      </template>
+      <template slot="footer">
+        <el-button type="primary" @click="exportExcel">确定导出</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </template>
+    </el-dialog>
   </home>
 </template>
 
 <script>
   import commonUtils from '../../common/commonUtils'
-
+  const rolesOptions = ['角色ID', '角色民称', '审批人账号', '审批人员工编号', '审批人姓名', '审批人所属部门', '状态', '描述'];
   export default {
     data() {
       return {
@@ -75,7 +89,7 @@
           name: '',
         },
         tableData: [],
-        RoleStatusEnum:{},
+        RoleStatusEnum: {},
         selection: '',
         id: '',
         roleId: '',
@@ -86,8 +100,17 @@
         departmentName: '',
         roleStatus: '',
         description: '',
-        isModify:true,
-        dialogVisible:false,
+        isModify: true,
+
+         exportDialogVisible: false,
+         checkAll: false,
+        checkRoles: [],
+        roles:rolesOptions,
+        isIndeterminate: true,
+         filterVal: [],
+         list: [],
+         disabled: true,
+        roleDtoList:[],
       }
     },
     activated() {
@@ -122,7 +145,8 @@
         }).then((result) => {
           self.tableData = result.page.list;
           self.total = result.page.totalCount;
-          self.RoleStatusEnum=result.RoleStatusEnum;
+          self.RoleStatusEnum = result.RoleStatusEnum;
+          self.roleDtoList=self.tableData;
           //self.form.name="dsf";
         }).catch(function (error) {
           commonUtils.Log("roleManage/querylist.do_:" + error);
@@ -136,7 +160,7 @@
       modifyRole(val) {
         //点击修改按钮，进入修改角色弹窗
         //this.dialogVisible=true;
-        this.$router.push({path:'/ModifyRole',query:{roleID:val}});
+        this.$router.push({path: '/ModifyRole', query: {roleID: val}});
       },
       deleteRole() {//删除角色信息
         this.$confirm('此操作将删除该角色, 是否继续?', '提示', {
@@ -146,7 +170,7 @@
         }).then(() => {
           var self = this;
           var param = {
-            selection:self.selection,
+            selection: self.selection,
           };
           self.$http.get('roleManage/updateStatus.do_', {
             params: param
@@ -163,12 +187,86 @@
           });
         });
       },
-      selectionActive(val){
-        this.isModify=false;
+      selectionActive(val) {
+        this.isModify = false;
       },
-      roleAssignPermission(){
+      roleAssignPermission() {
         this.$router.replace("/roleManagement/RoleAssignPermission")
-      }
+      },
+
+
+      exportExcel() {
+        if (this.checkRoles.length === 0) {
+          this.$message({
+            showClose: false,
+            message: '请选择需要导出的字段',
+            type: 'error'
+          });
+        } else {
+          require.ensure([], () => {
+            const {export_json_to_excel} = require('../../excel/Export2Excel');
+            const tHeader = this.checkRoles;
+            // 上面设置Excel的表格第一行的标题
+
+            const filterVal = this.exportField(this.checkRoles);
+            // 上面的staffNum、accountId、staffName是tableData里对象的属性
+            const list = this.roleDtoList;  //把data里的tableData存到list
+            for (let i = 0; i < list.length; i++) {
+              list[i].roleStatus=this.RoleStatusEnum[list[i].roleStatus];
+            }
+            const data = this.formatJson(filterVal, list);
+            export_json_to_excel(tHeader, data, '员工管理列表excel');
+            this.$message({
+              showClose: true,
+              message: '文件导出成功',
+              type: 'success'
+            });
+            this.exportDialogVisible = false;
+            this.checkRoles = [];
+            this.filterVal = [];
+          })
+        }
+      },
+      cancel() {
+        this.exportDialogVisible = false;
+      },
+      exportRole() {
+        this.exportDialogVisible = true;
+      },
+      exportField(val) {
+        for (let i = 0; i < val.length; i++) {
+          if (this.checkRoles[i] === '角色ID') {
+            this.filterVal.push('roleId')
+          } else if (this.checkRoles[i] === '角色名称') {
+            this.filterVal.push('roleName')
+          } else if (this.checkRoles[i] === '审批人账号') {
+            this.filterVal.push('accountNum')
+          } else if (this.checkRoles[i] === '审批人员编号') {
+            this.filterVal.push('staffId')
+          } else if (this.checkRoles[i] === '审批人姓名') {
+            this.filterVal.push('staffName')
+          } else if (this.checkRoles[i] === '审批人所属部门') {
+            this.filterVal.push('departmentName')
+          } else if (this.checkRoles[i] === '状态') {
+            this.filterVal.push('roleStatus')
+          } else if (this.checkRoles[i] === '描述') {
+            this.filterVal.push('description')
+          }
+        }
+        return this.filterVal;
+      },
+      formatJson(filterVal, jsonData) {
+        return jsonData.map(v => filterVal.map(j => v[j]))
+      },
+      handleCheckAllChange(val) {
+        this.checkRoles = val ? rolesOptions : [];
+        this.isIndeterminate = false;
+      },
+      handlecheckRolesChange(value) {
+        let checkedCount = value.length;
+        this.checkAll = checkedCount === this.roles.length;
+        this.isIndeterminate = checkedCount > 0 && checkedCount < this.roles.length;
+      },
     }
   }
 </script>
