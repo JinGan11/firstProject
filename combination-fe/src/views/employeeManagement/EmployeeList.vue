6d28a7faf833bd510be260d03bdabd3d35c86624
jing.luo01@ucarinc.com <jing.luo01@ucarinc.com>
2019-08-08 19:34:21 +0800

    分配部门前端

--- combination-fe/src/views/employeeManagement/EmployeeList.vue
+++ combination-fe/src/views/employeeManagement/EmployeeList.vue
@@ -153,7 +153,7 @@
             <el-input v-model="formdiStributionDepartment.staffBeforeDepartment" placeholder="请输入当前部门"></el-input>
           </el-form-item>
           <el-form-item label="调整后部门">
-            <el-input v-model="formdiStributionDepartment.staffAfterDepartment" placeholder="请输入归属部门"></el-input>
+            <el-input style="width: 170px;" v-model="formdiStributionDepartment.staffAfterDepartment" placeholder="请输入归属部门"></el-input><el-button style="width: 40px" typ="primary" @click="departmentVisible=true">选择</el-button>
           </el-form-item>
         </el-form>
       </div>
@@ -174,6 +174,20 @@
         <el-button type="primary" @click="exportExcel">确定导出</el-button>
         <el-button @click="cancel">取 消</el-button>
       </template>
+    </el-dialog>
+    <el-dialog title="请选择部门" :visible.sync="departmentVisible" :close-on-click-modal="false" width="600px">
+      <el-tree
+        ref="tree"
+        :props="defaultProps"
+        node-key="id"
+        :load="loadNode"
+        :lazy="true"
+        check-strictly
+        show-checkbox
+        :render-content="renderContent"
+        @check-change="handleClick">
+      </el-tree>
+      <el-button @click="getCheckedNodes">点击弹出选中的部门的ID</el-button>
     </el-dialog>
   </home>
 
@@ -262,6 +276,14 @@
         filterVal: [],
         list: [],
         disabled: true,
+        departmentVisible:false,
+        defaultProps: {
+          label: 'departmentName',
+          children: 'children',
+          id: 'id',
+          status: 'status'
+        },
+        operationBtnActive: true,
       }
     },
     filters: {
@@ -449,6 +471,7 @@
       },
       distributionDepartment() {
         this.distributionDepartmentFlag = true;
+        this.departmentVisible=false;
       },
       saveDepartment() {
 
@@ -557,6 +580,88 @@
         this.id = val.id;
         this.accountId = val.accountId;
 
+      },
+      loadNode(node,resolve){
+        var self = this;
+        self.$http.get('department/buildTree.do_', {
+          params: null
+        }).then((result) => {
+          resolve([result.departmentDto]);
+        }).catch(function (error) {
+
+        });
+      },
+      getCheckedNodes() {
+        // 用于演示获取部门id的方式
+        alert(this.$refs.tree.getCheckedNodes()[0].id);
+      },
+      handleClick(data,checked,node){
+        // 手动设置单选
+        if(checked === true) {
+          this.checkedId = data.id;
+          this.$refs.tree.setCheckedKeys([data.id]);
+          // 设置按钮是否可选（选中节点后调用两次handleClick，第一次checked为true，所以设置按钮写在这）
+          if(data.status === 1){
+            this.operationBtnActive=false;
+          }else{
+            this.operationBtnActive=true;
+          }
+        } else {
+          if (this.checkedId == data.id) {
+            this.$refs.tree.setCheckedKeys([data.id]);
+          }
+        }
+      },
+      deleteDept(){
+        var check_node = this.$refs.tree.getCheckedNodes()[0];
+        if( this.$options.methods.checkHaveChildren(check_node) ){
+          alert("删除失败，请先删除该部门的下级部门");
+          return;
+        }
+        var params = {
+          id: check_node.id
+        };
+        this.$http.post('department/deleteDepartment.do_',params);
+        this.dialogVisible = false;
+        // 删除节点后前端显示跟着修改，避免刷新整个页面
+        check_node.status = 0;
+        var tmpNode = {
+          departmentName: "tmp",
+          children: [],
+          id: 99999999,
+          status: 0
+        };
+        this.$refs.tree.insertBefore(tmpNode,check_node.id);
+        this.$refs.tree.remove(check_node.id);
+        this.$refs.tree.insertAfter(check_node,tmpNode.id);
+        this.$refs.tree.remove(tmpNode.id);
+        this.operationBtnActive=true;
+        alert("部门删除成功");
+      },
+      checkHaveChildren (data) {
+        for(var i = 0;i<data.children.length;i++){
+          if(data.children[i].status === 1){
+            return true;
+          }
+        }
+        return false;
+      },
+      renderContent(h, { node, data, store }) {
+        // 这里编译器有红色波浪线不影响运行...
+        if(data.status != 1){
+          return (
+            <span style="color:red">{node.label}</span>
+        );
+        }else{
+          return (
+            <span>{node.label}</span>
+        );
+        }
+      },
+      changeUpper() {
+        window.localStorage.setItem("dept_id",this.$refs.tree.getCheckedNodes()[0].id);
+        window.localStorage.setItem("dept_name",this.$refs.tree.getCheckedNodes()[0].departmentName);
+        this.$router.replace('/departmentManagement/changeUpperDepartment');
       }
     }
   }
