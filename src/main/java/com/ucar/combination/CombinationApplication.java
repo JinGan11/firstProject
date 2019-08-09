e12dd77e41da1eae0381712a3b9063ca0c8492fe
蓝庆裕 <qingyu.lan@ucarinc.com>
2019-08-09 14:26:10 +0800

    打包springboot项目用

--- src/main/java/com/ucar/combination/CombinationApplication.java
+++ src/main/java/com/ucar/combination/CombinationApplication.java
@@ -2,15 +2,22 @@ package com.ucar.combination;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @ServletComponentScan
-public class CombinationApplication {
+public class CombinationApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(CombinationApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder springApplicationBuilder){
+		return springApplicationBuilder.sources(this.getClass());
 	}
 
 }
