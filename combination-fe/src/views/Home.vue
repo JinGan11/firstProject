00291f7070dc1c34a33659db30ee382d0fb40161
peng.zhang11 <peng.zhang11@ucarinc.com>
2019-08-14 14:44:21 +0800

    修改样式

--- combination-fe/src/views/Home.vue
+++ combination-fe/src/views/Home.vue
@@ -32,6 +32,8 @@
     overflow: hidden;
     height: 100%;
     width: 100%;
+    margin-top: -15px;
+    margin-left: -15px;
     background-size: cover;
     background-repeat: no-repeat;
     background-image: url("../../static/img/home.jpg");
