d43899a9b6a6fb1b90eef441a260605921089bcb
jing.luo01@ucarinc.com <jing.luo01@ucarinc.com>
2019-08-16 17:41:01 +0800

    全部页面的导出功能细节
    (cherry picked from commit 2d6452296a8ebc3f64d0b116e75fa9c176d9de5e)

--- combination-fe/src/views/cityInfoManagement/citySearch.vue
+++ combination-fe/src/views/cityInfoManagement/citySearch.vue
@@ -267,8 +267,21 @@
                             list[i].regionStatus = '有效'
                         }
                     }
-                    const data = this.formatJson(filterVal, list);
-                    export_json_to_excel(tHeader, data, '省市管理列表excel');
+                  //获取当前时间
+                  var date = new Date();
+                  var seperator1 = "-";
+                  var year = date.getFullYear();
+                  var month = date.getMonth() + 1;
+                  var strDate = date.getDate();
+                  if (month >= 1 && month <= 9) {
+                    month = "0" + month;
+                  }
+                  if (strDate >= 0 && strDate <= 9) {
+                    strDate = "0" + strDate;
+                  }
+                  var currentdate = year + seperator1 + month + seperator1 + strDate;
+                  const data = this.formatJson(filterVal, list);
+                    export_json_to_excel(tHeader, data, '城市查询'+currentdate);
                     this.$message({
                         showClose: true,
                         message: '文件导出成功',
--- combination-fe/src/views/employeeManagement/EmployeeList.vue
+++ combination-fe/src/views/employeeManagement/EmployeeList.vue
@@ -1139,8 +1139,21 @@
                 list[i].staffSex = '女'
               }
             }
+            //获取当前时间
+            var date = new Date();
+            var seperator1 = "-";
+            var year = date.getFullYear();
+            var month = date.getMonth() + 1;
+            var strDate = date.getDate();
+            if (month >= 1 && month <= 9) {
+              month = "0" + month;
+            }
+            if (strDate >= 0 && strDate <= 9) {
+              strDate = "0" + strDate;
+            }
+            var currentdate = year + seperator1 + month + seperator1 + strDate;
             const data = this.formatJson(filterVal, list);
-            export_json_to_excel(tHeader, data, '员工管理列表excel');
+            export_json_to_excel(tHeader, data, '员工管理'+currentdate);
             this.$message({
               showClose: true,
               message: '文件导出成功',
