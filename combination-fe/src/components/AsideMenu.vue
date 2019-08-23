1c0dcd4abef005c2a951d6a61cf72387a8159cbc
junqiang.zhang@ucarinc.com <junqiang.zhang@ucarinc.com>
2019-08-23 13:49:18 +0800

    菜单样式

--- combination-fe/src/components/AsideMenu.vue
+++ combination-fe/src/components/AsideMenu.vue
@@ -1,13 +1,17 @@
 <template>
   <div>
-    <el-aside width="auto" class="aside" >
+    <el-aside width="auto" style="height:100%;">
       <el-menu :default-active="$route.path"
                router
+               style="height:100%;"
                class="el-menu-vertical-demo"
                :collapse="isCollapse"
                unique-opened>
-        <div class="aside-title" style="background: #1E90FF ;font-size: 28px;font-family: STKaiti">优车组织结构</div>
-        <template v-for="item in data">
+        <el-header>
+          <div class="aside-title" style="width:100% ;position: absolute;right:0px ;background: #1E90FF ;font-size: 28px;font-family: STKaiti">优车组织结构</div>
+        </el-header>
+        <el-aside width="auto" style="position: absolute;left:0px;top:65px ;bottom: 0px; right:0px">
+          <template v-for="item in data">
           <el-menu-item v-if="item.children === null" :index="item.url">
             <i :class="item.icon"></i>
             <span slot="title">{{item.title}}</span>
@@ -22,6 +26,7 @@
             </el-menu-item-group>
           </el-submenu>
         </template>
+        </el-aside>
         <!--<MenuItem :data="data"></MenuItem>-->
       </el-menu>
     </el-aside>
