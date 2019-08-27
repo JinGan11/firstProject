73690faf7c826ca77b10b165c5c993d3a28e8370
peng.zhang11 <peng.zhang11@ucarinc.com>
2019-08-27 08:28:25 +0800

    账号密码不能为空格

--- combination-fe/src/App.vue
+++ combination-fe/src/App.vue
@@ -93,13 +93,13 @@
           <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" size="medium" label-width="100px"
                    class="demo-ruleForm">
             <el-form-item label="旧密码" prop="oldPass">
-              <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.oldPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item label="新密码" prop="newPass">
-              <el-input type="password" v-model="ruleForm.newPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.newPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item label="确认新密码" prop="checkPass">
-              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.checkPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item style="text-align: center">
               <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
@@ -113,13 +113,13 @@
           <el-form :model="ruleForm" status-icon :rules="rules" ref="ruleForm" size="medium" label-width="100px"
                    class="demo-ruleForm">
             <el-form-item label="旧密码" prop="oldPass">
-              <el-input type="password" v-model="ruleForm.oldPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.oldPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item label="新密码" prop="newPass">
-              <el-input type="password" v-model="ruleForm.newPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.newPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item label="确认新密码" prop="checkPass">
-              <el-input type="password" v-model="ruleForm.checkPass" autocomplete="off" style="width: 70%"></el-input>
+              <el-input type="password" v-model.trim="ruleForm.checkPass" autocomplete="off" style="width: 70%"></el-input>
             </el-form-item>
             <el-form-item style="text-align: center">
               <el-button type="primary" @click="submitForm('ruleForm')">提交</el-button>
